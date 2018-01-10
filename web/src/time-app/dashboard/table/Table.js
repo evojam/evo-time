a0923d8bbb2467677ee34769785ffd4c229c08d3
Krystian Pudlik <kpudlik@evojam.com>
2018-01-10 18:10:31 +0100

    Connect table with selector

--- web/src/time-app/dashboard/table/Table.js
+++ web/src/time-app/dashboard/table/Table.js
@@ -4,14 +4,21 @@ import { SummaryTable } from './SummaryTable'
 import { DaysTable } from './DaysTable'
 
 import './Table.css'
+import connect from 'react-redux/es/connect/connect'
 
-export const Table = () => (
+export const Table = ({ month }) => (
   <div className="dashboard-table">
     <div className="summary-table-container">
       <SummaryTable />
     </div>
     <div className="days-table-container">
-      <DaysTable month={new Date(2018, 0)} />
+      <DaysTable month={month} />
     </div>
   </div>
 )
+
+const mapStateToProps = state => ({
+  month: state.selectedPeriod[0] // TODO: Temporary, it works only for fullmonths
+})
+
+export default connect(mapStateToProps, null)(Table)
