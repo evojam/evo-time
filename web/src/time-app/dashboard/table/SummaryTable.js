8d6940a9aafdcc1ae121b32dc90710be46ed651f
JustynaB <justyna@evojam.com>
2018-01-10 14:40:00 +0100

    Add dashboard summary table

--- web/src/time-app/dashboard/table/SummaryTable.js
+++ web/src/time-app/dashboard/table/SummaryTable.js
@@ -1,4 +1,21 @@
 import React from 'react'
+import './SummaryTable.css'
 
-export const SummaryTable = ({ className }) =>
-  <div className={className}>summary table</div>
+export const SummaryTable = ({usersSummary, className}) => (
+    <table className={`aui ${className}`}>
+      <thead>
+        <tr>
+          <th>User</th>
+          <th>Sum</th>
+        </tr>
+      </thead>
+      <tbody>
+      {usersSummary.map(user =>
+        <tr key={user.userName}>
+          <td className="summary-table__cell">{user.userName}</td>
+          <td className="summary-table__cell">{user.sumOfHours}</td>
+        </tr>
+      )}
+      </tbody>
+    </table>
+  )
