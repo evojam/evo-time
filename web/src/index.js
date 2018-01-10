02ad206fd2438ac44b9ded597b42ec7aa0d80884
Marcin Wadon <marcin@wadon.net>
2018-01-10 12:18:57 +0100

    React bootstrap

---
+++ web/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {App} from 'time-app/App'
+
+// TODO: import bootstrap, etc. here
+
+ReactDOM.render(<App />, document.getElementById('root'))
---
+++ web/src/time-app/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+// import { Provider } from 'react-redux'
+
+// import { store } from 'time-lib/store'
+// import { Router } from './Router'
+
+export const App = () => (
+  <div>123</div>
+)
