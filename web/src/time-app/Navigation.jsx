7d21409f2bfcd24019b394894b6d8ed55e597761
Krystian Pudlik <kpudlik@evojam.com>
2018-01-10 12:40:19 +0100

    Create basic layout

---
+++ web/src/time-app/Navigation.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const Navigation = () => (
+  <nav>ARROWS AND (in the future) DATEPICKER COMES HERE</nav>
+)
---
+++ web/src/time-app/Table/Table.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const Table = () => (
+  <nav>TABLE COMES HERE</nav>
+)
