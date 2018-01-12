7a68ac33466b995d63958470eb077994df5af454
Krystian Pudlik <kpudlik@evojam.com>
2018-01-12 12:37:25 +0100

    Fix grouping

--- web/src/time-lib/worklogs/selectors.js
+++ web/src/time-lib/worklogs/selectors.js
@@ -1,14 +1,20 @@
-import { sumWorklogInSeconds } from '../date-and-time'
 import { Maybe } from 'monet'
 import { OrderedMap } from 'immutable'
 
-export const groupByIssue = worklog => worklog.issue.key
+export const groupByIssue = worklog => worklog.project.id
 
 const toUserWorklogByDate = date => user => Maybe.fromNull(user.worklogs.get(date))
 
 const toWorklogsPerProject = worklog => worklog
   .groupBy(groupByIssue)
-  .map(sumWorklogInSeconds)
+  .map(worklogs => worklogs
+    .reduce(({ projectName, totalSeconds }, worklog) => {
+      return {
+        projectName: worklog.project.name,
+        totalSeconds: totalSeconds + worklog.timeSpentInSeconds
+      }
+    }, { projectName: null, totalSeconds: 0 })
+  )
 
 export const getSummaryForEachProject = (worklogs, username, date) => {
   return Maybe.fromNull(worklogs.get(username))
