b096d104fecd2a917647ee0d298e5b0b36f409c0
Marcin Wadon <marcin@wadon.net>
2018-01-12 13:34:39 +0100

    Holidays

--- web/src/time-lib/worklogs/epic.js
+++ web/src/time-lib/worklogs/epic.js
@@ -1,16 +1,18 @@
-import { map, switchMap, catchError } from 'rxjs/operators'
+import { map, flatMap, switchMap, catchError } from 'rxjs/operators'
 import { of } from 'rxjs/observable/of'
 import { ofType, combineEpics } from 'redux-observable'
-import { format } from 'date-fns'
+import { format, startOfYear } from 'date-fns'
 
 import { ajax } from '../api'
 
 import {
   getWorklogSuccess, getWorklogFailure, getWorklog,
+  getHolidays, getHolidaysSuccess, getHolidaysFailure,
   WorklogActionType,
 } from './actions'
 
 const toStringFormat = date => format(date, 'YYYY-MM-DD')
+const toYearFormat = date => format(date, 'YYYY')
 
 const previousPeriodEpic = (action$, store) => action$.pipe(
   ofType(WorklogActionType.PreviousPeriod),
@@ -35,8 +37,36 @@ const fetchWorklogsEpic = (action$, store) => action$.pipe(
   })
 )
 
+const fetchHolidaysOnWorklogsActionEpic = (action$, store) => action$.pipe(
+  ofType(WorklogActionType.GetWorklog),
+  flatMap(() => {
+    const state = store.getState()
+    const [year] = state.selectedPeriod.map(startOfYear)
+    const holidays = state.holidays
+
+    return holidays.has(year)
+      ? []
+      : [getHolidays(year)]
+  })
+)
+
+const fetchHolidaysEpic = (action$, store) => action$.pipe(
+  ofType(WorklogActionType.GetHolidays),
+  switchMap(() => {
+    const state = store.getState()
+    const [year] = state.selectedPeriod.map(toYearFormat)
+
+    return ajax.get(`holidays?year=${year}`).pipe(
+      map(getHolidaysSuccess(year)),
+      catchError(() => of(getHolidaysFailure()))
+    )
+  })
+)
+
 export const worklogEpic = combineEpics(
   previousPeriodEpic,
   nextPeriodEpic,
   fetchWorklogsEpic,
+  fetchHolidaysOnWorklogsActionEpic,
+  fetchHolidaysEpic,
 )
